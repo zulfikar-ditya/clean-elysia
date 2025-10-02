1ca21c9a25d450074380119142d25fe2ecccf4f6
zulfikar-ditya <zulfikarditya.work@gmail.com>
2025-10-02 10:31:42 +0700

    feat: separate route to different file

--- apps/apis/routes/app.routes.ts
+++ apps/apis/routes/app.routes.ts
@@ -1,71 +1,16 @@
 import Elysia from "elysia";
 import { HomeHandler } from "@apis/handlers/home.handler";
-import { AuthHandler } from "@apis/handlers/auth.handler";
-import { authMiddleware } from "@apis/middleware";
-import { ProfileHandler } from "@apis/handlers/profile.handler";
-import { AppContext } from "../types/elysia";
-import { PermissionHandler } from "@apis/handlers/settings/permission.handler";
-import { RoleHandler } from "../handlers/settings/role.handler";
-import { SettingSelectHandler } from "../handlers/settings/select-options/select.handler";
-import { UserHandler } from "../handlers/settings/user.handler";
-import { roleMiddleware } from "../middleware/role.middleware";
+import { authRoutes } from "./auth.routes";
+import { profileRoutes } from "./profile.routes";
+import { settingRoutes } from "./setting.routes";
 
 const routes = new Elysia();
 
 routes.get("/", HomeHandler.home);
 routes.get("/health", HomeHandler.health);
 
-// authenticate
-routes.post("/auth/login", AuthHandler.login);
-routes.post("/auth/register", AuthHandler.register);
-routes.post("/auth/verify-email", AuthHandler.verifyEmail);
-routes.post(
-	"/auth/resent-verification-email",
-	AuthHandler.resentVerificationEmail,
-);
-
-// AUTHENTICATE HANDLER
-routes.group("", (app) => {
-	app.derive(async (ctx) => {
-		await authMiddleware(ctx as AppContext);
-		return {};
-	});
-
-	app.get("/profile", ProfileHandler.profile);
-	app.patch("/profile", ProfileHandler.updateProfile);
-	app.patch("/profile/password", ProfileHandler.updatePassword);
-
-	app.group("/settings", (settingRoutes) => {
-		settingRoutes.derive((ctx) => {
-			roleMiddleware(ctx as AppContext, []);
-			return {};
-		});
-
-		settingRoutes.get("/permission", PermissionHandler.list);
-		settingRoutes.post("/permission", PermissionHandler.create);
-		settingRoutes.get("/permission/:id", PermissionHandler.detail);
-		settingRoutes.patch("/permission/:id", PermissionHandler.update);
-		settingRoutes.delete("/permission/:id", PermissionHandler.delete);
-
-		settingRoutes.get("/role", RoleHandler.list);
-		settingRoutes.post("/role", RoleHandler.create);
-		settingRoutes.get("/role/:id", RoleHandler.detail);
-		settingRoutes.patch("/role/:id", RoleHandler.update);
-		settingRoutes.delete("/role/:id", RoleHandler.delete);
-
-		settingRoutes.get("/user", UserHandler.list);
-		settingRoutes.post("/user", UserHandler.create);
-		settingRoutes.get("/user/:id", UserHandler.detail);
-		settingRoutes.patch("/user/:id", UserHandler.update);
-		settingRoutes.delete("/user/:id", UserHandler.delete);
-
-		settingRoutes.get("/select/permission", SettingSelectHandler.permissions);
-		settingRoutes.get("/select/role", SettingSelectHandler.roles);
-
-		return settingRoutes;
-	});
-
-	return app;
-});
+routes.use(authRoutes);
+routes.use(profileRoutes);
+routes.use(settingRoutes);
 
 export default routes;
