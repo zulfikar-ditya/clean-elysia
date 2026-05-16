2b3d91765039607ad037786c6cb82459b2fccb56
zulfikar-ditya <zulfikarditya.work@gmail.com>
2026-05-16 12:38:34 +0700

    fix: eslint

--- src/libs/database/postgres/migrations/relations.ts
+++ src/libs/database/postgres/migrations/relations.ts
@@ -1,53 +1,65 @@
 import { relations } from "drizzle-orm/relations";
-import { user, userEmailVerification, passwordReset, userRole, role, rolePermission, permission } from "./schema";
 
-export const userEmailVerificationRelations = relations(userEmailVerification, ({one}) => ({
-	user: one(user, {
-		fields: [userEmailVerification.userId],
-		references: [user.id]
+import {
+	passwordReset,
+	permission,
+	role,
+	rolePermission,
+	user,
+	userEmailVerification,
+	userRole,
+} from "./schema";
+
+export const userEmailVerificationRelations = relations(
+	userEmailVerification,
+	({ one }) => ({
+		user: one(user, {
+			fields: [userEmailVerification.userId],
+			references: [user.id],
+		}),
 	}),
-}));
+);
 
-export const userRelations = relations(user, ({many}) => ({
+export const userRelations = relations(user, ({ many }) => ({
 	userEmailVerifications: many(userEmailVerification),
 	passwordResets: many(passwordReset),
 	userRoles: many(userRole),
 }));
 
-export const passwordResetRelations = relations(passwordReset, ({one}) => ({
+export const passwordResetRelations = relations(passwordReset, ({ one }) => ({
 	user: one(user, {
 		fields: [passwordReset.userId],
-		references: [user.id]
+		references: [user.id],
 	}),
 }));
 
-export const userRoleRelations = relations(userRole, ({one}) => ({
+export const userRoleRelations = relations(userRole, ({ one }) => ({
 	user: one(user, {
 		fields: [userRole.userId],
-		references: [user.id]
+		references: [user.id],
 	}),
 	role: one(role, {
 		fields: [userRole.roleId],
-		references: [role.id]
+		references: [role.id],
 	}),
 }));
 
-export const roleRelations = relations(role, ({many}) => ({
+export const roleRelations = relations(role, ({ many }) => ({
 	userRoles: many(userRole),
 	rolePermissions: many(rolePermission),
 }));
 
-export const rolePermissionRelations = relations(rolePermission, ({one}) => ({
+export const rolePermissionRelations = relations(rolePermission, ({ one }) => ({
 	role: one(role, {
 		fields: [rolePermission.roleId],
-		references: [role.id]
+		references: [role.id],
 	}),
 	permission: one(permission, {
 		fields: [rolePermission.permissionId],
-		references: [permission.id]
+		references: [permission.id],
 	}),
 }));
 
-export const permissionRelations = relations(permission, ({many}) => ({
+export const permissionRelations = relations(permission, ({ many }) => ({
 	rolePermissions: many(rolePermission),
 }));
